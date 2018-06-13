ac839b280ed55a0cf623f961d93e235ee3448438
Baiwei Yu <baiwei@ada.cs.pdx.edu>
2018-06-13 01:44:37 -0700

    Modify app.py, presenter.py and course_2.html to support Google Translate. Also add my name on index.html

--- final/app.py
+++ final/app.py
@@ -5,17 +5,19 @@ import requests
 
 
 from presenter import Presenter
+#from presenter_2 import Presenter_2
 from model import AppModel
 
 import argparse
 import sys
 
-from google.cloud import language
-from google.cloud import translate
+from google.cloud import language #Google sentiment analysis API, used by Taisheng Guo
+from google.cloud import translate #Goole Translate API, used by Baiwei Yu
 from google.cloud.language import enums
 from google.cloud.language import types
 import six
 
+#--------- Sentiment analysis API, by Taisheng Guo's --------
 def sentiment_text(text):
     """Detects sentiment in the text."""
     client = language.LanguageServiceClient()
@@ -46,9 +48,10 @@ def sentiment_text(text):
     else:
         return 'Bad'
     # [END migration_analyze_sentiment]
+# ------------------- End of sentiment API  ----------------
 
 
-
+#------- Google Translate API, by Baiwei Yu ----------------
 def translate_text(target, text):
     # [START translate_translate_text]
     """Translates text into the target language.
@@ -72,11 +75,14 @@ def translate_text(target, text):
     #print(u'Detected source language: {}'.format(
         #result['detectedSourceLanguage']))
     # [END translate_translate_text]
-    
+#---------------- End of Google Tranlate API ----------------    
 
 
 app = Flask(__name__)
 model = AppModel()
+trans_model = AppModel() #conent translated in Chinese
+
+
 value1 = ('CS410 Web Security', 'Winter 2018', 'TR 14:00~15:50', 'Wu-chang Feng', '4.5/5', '3.5/5', "Web Security was fun and I learned a ton! This should be a required class. Wu Chang does and excellent job teaching it as well and is very helpful in and out of class. Not a tough grader and lenient if you talk to him about missing classes. There is a ton of work though for this class, so be prepared to put in the time! I'm happy I took this class!")
 
 value2 = ('CS201', 'Fall 2014', 'MW 10:00~11:50', 'Wu-chang Feng', '3/5', '3/5', "Professor Feng was a great professor. He is extremely knowledgeable, welcomes questions, and loves his material. The CTF homework makes the class enjoyable and was a good learning tool. Be prepared to spend a lot of time looking at assembly code.")
@@ -85,41 +91,48 @@ value3 = ('CS202', 'Fall 2018', '13:00~15:50', 'Karla Fant', '5/5', '4/5', "Took
 
 value4 = ('CS999', 'Fall 2028', '7:00~8:50', 'Unknow', '1/5', '1/5', "This class is very horrible. Tons of hw and exams, won't ever take it again!!!")
 
-
-#sentiment_text(value2[6])
-#sentiment_text(value3[6])
-#sentiment_text(value4[6])
-
-trans_value1 = ()
-
-for x in value1:
-	temp = translate_text('zh', x)
-	trans_value1.append(temp)
-
-
-
-
-
-
-print(trans_value1)
-'''
+#------ Implement of sentiment analysis API, by Taisheng Guo ------
 value1 = value1 + (sentiment_text(value1[6]),)
 value2 = value2 + (sentiment_text(value2[6]),)
 value3 = value3 + (sentiment_text(value3[6]),)
 value4 = value4 + (sentiment_text(value4[6]),)
+#------------------------------- end ------------------------------
+
+#------ Implement of Google Translate API, by Baiwei Yu -----------
+trans_value1 = ()
+trans_value2 = ()
+trans_value3 = ()
+trans_value4 = ()
+
+for x in value1:
+	trans_value1 = trans_value1 +  (translate_text('zh', x),)
+for x in value2:
+	trans_value2 = trans_value2 +  (translate_text('zh', x),)
+for x in value3:
+	trans_value3 = trans_value3 +  (translate_text('zh', x),)
+for x in value4:
+	trans_value4 = trans_value4 +  (translate_text('zh', x),)
+#------------------------------ end -------------------------------
+
 
 model.insert_course(value1)
 model.insert_course(value2)
 model.insert_course(value3)
 model.insert_course(value4)
-'''
+
+#--- Add translated content to new model,by Baiwei Yu ----
+trans_model.insert_course(trans_value1)
+trans_model.insert_course(trans_value2)
+trans_model.insert_course(trans_value3)
+trans_model.insert_course(trans_value4)
+#---------------------------------------------------------
 
 '''
 model.insert_course(model, value1)
 model.insert_course(model, value2)
 model.insert_course(model, value3)
 '''
-presenter = Presenter(model)
+presenter = Presenter(model, trans_model) #presenter is modified to accepting two models. by Taisheng Guo
 
 def create_template(route):
     route_name = route.get_name()
@@ -149,5 +162,13 @@ def base():
 def course():
     return render_view(presenter.course())
 
+#----- Translated content is passed to course_2.html. by Taisheng Guo----
+@app.route('/course_2')
+def course_2():
+    return render_view(presenter.course_2())
+#-------- end----------------------------------
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
+
+
--- final/presenter.py
+++ final/presenter.py
@@ -8,8 +8,11 @@ from view_header import Route, PresentView, Flash, MSG_TYPE
 
 class Presenter:
 
-    def __init__(self, model):
+#----- model is in English, trans_mode is in Chinese ------
+    def __init__(self, model, trans_model):
         self.model = model
+        self.trans_model = trans_model
+#---------------------- end ------------------------
 
 
     def index(self):
@@ -21,7 +24,7 @@ class Presenter:
         courses = self.model.fetchall()
 
         if courses[0]:
-            args = {'courses':courses}
+            args = {'courses':courses[:4]}
             route = Route(False, 'course.html', args)
             return PresentView(route)
         else:
@@ -30,3 +33,18 @@ class Presenter:
             route = Route(False, 'course.html', args)
             return PresentView(route)
 
+#-------- This is prepared for course_2.html -----------
+    def course_2(self):
+        '''courses = self.model.fetchall(self.model)'''
+        courses_2 = self.trans_model.fetchall()
+
+        if courses_2[0]:
+            args = {'courses':courses_2[4:]}
+            route = Route(False, 'course_2.html', args)
+            return PresentView(route)
+        else:
+            msg = 'No Course Found'
+            args = {'msg':msg}
+            route = Route(False, 'course_2.html', args)
+            return PresentView(route)
+#---------------------- end ----------------------------
